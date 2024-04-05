0946fe1b5b94610ecc39883fbed3e60989214a67
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2024-04-05 21:02:16 +0000

    Update test conveyor.ino

--- test conveyor.ino
+++ test conveyor.ino
@@ -1,1 +1,29 @@
+#define relay 5
 
+void setup() {
+  pinMode(relay, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) { 
+    char command = Serial.read(); 
+    
+    switch (command) {
+      case '0': // Stop the motor
+        stopMotor();
+        break;
+      case '1': // Turn motor in one direction
+        runMotor();
+        break;
+      }
+  }
+}
+
+void stopMotor() {
+  digitalWrite(relay, LOW);
+}
+void runMotor() {
+  digitalWrite(relay, HIGH);
+}
