3c73551bc5bc8757cff83de350cdf8f24b074ace
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2024-04-11 14:43:25 +0000

    Update color sensor read serial.ino

--- color sensor read serial.ino
+++ color sensor read serial.ino
@@ -1,61 +1,110 @@
+/*
+  RGB Color Sensor Demonstration
+  rgb-color-sensor-demo.ino
+  Read RGB values from Color Sensor
+  Must use calibration values from Color Sensor Calibration Sketch
+
+  DroneBot Workshop 2020
+  https://dronebotworkshop.com
+*/
+
 // Define color sensor pins
+
 #define S0 4
 #define S1 5
 #define S2 6
 #define S3 7
 #define sensorOut 8
 
+// Calibration Values
+// Get these from Calibration Sketch
+
+int redMin = 96; // Red minimum value
+int redMax = 411; // Red maximum value
+int greenMin = 111; // Green minimum value
+int greenMax = 445; // Green maximum value
+int blueMin = 87; // Blue minimum value
+int blueMax = 455; // Blue maximum value
+
 // Variables for Color Pulse Width Measurements
+
 int redPW = 0;
 int greenPW = 0;
 int bluePW = 0;
 
+// Variables for final Color values
+
+int redValue;
+int greenValue;
+int blueValue;
+
+
+
+int minsensingvalue = 100;
 void setup() {
+
   // Set S0 - S3 as outputs
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
-
-  // Set Pulse Width scaling to 20%
-  digitalWrite(S0,HIGH);
-  digitalWrite(S1,LOW);
-
+  
   // Set Sensor output as input
   pinMode(sensorOut, INPUT);
-
+  
+  // Set Frequency scaling to 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+  
   // Setup Serial Monitor
   Serial.begin(9600);
 }
 
 void loop() {
-  // Read Red Pulse Width
+  
+  // Read Red value
   redPW = getRedPW();
+  // Map to value from 0-255
+  redValue = map(redPW, redMin, redMax, 255, 0);
   // Delay to stabilize sensor
   delay(200);
-
-  // Read Green Pulse Width
+  
+  // Read Green value
   greenPW = getGreenPW();
+  // Map to value from 0-255
+  greenValue = map(greenPW, greenMin, greenMax, 255, 0);
   // Delay to stabilize sensor
   delay(200);
-
-  // Read Blue Pulse Width
+  
+  // Read Blue value
   bluePW = getBluePW();
+  // Map to value from 0-255
+  blueValue = map(bluePW, blueMin, blueMax, 255, 0);
   // Delay to stabilize sensor
   delay(200);
-
-  // Print output to Serial Monitor
-  Serial.print("Red PW = ");
-  Serial.print(redPW);
-  Serial.print(" - Green PW = ");
-  Serial.print(greenPW);
-  Serial.print(" - Blue PW = ");
-  Serial.println(bluePW);
+  
+  // Comparisons to determine the dominant color
+  if(redValue > greenValue && redValue > blueValue && redValue >= minsensingvalue) {
+    Serial.println("Red");
+  } else if(greenValue > redValue && greenValue > blueValue && greenValue >= minsensingvalue) {
+    Serial.println("Green");
+  } else if(blueValue > redValue && blueValue > greenValue && blueValue >= minsensingvalue) {
+    Serial.println("Blue");
+  } else if(redValue < minsensingvalue && greenValue < minsensingvalue && blueValue < minsensingvalue) {
+    // If all values are below 130, do nothing (no Serial print)
+  } else {
+    // Optional: Handle cases where two colors are equal and above 130,
+    // or no single color dominates significantly.
+    // For simplicity, this is left out, but you could add additional conditions here.
+  }
+  
 }
+
 
 
 // Function to read Red Pulse Widths
 int getRedPW() {
+
   // Set sensor to read Red only
   digitalWrite(S2,LOW);
   digitalWrite(S3,LOW);
@@ -65,10 +114,12 @@ int getRedPW() {
   PW = pulseIn(sensorOut, LOW);
   // Return the value
   return PW;
+
 }
 
 // Function to read Green Pulse Widths
 int getGreenPW() {
+
   // Set sensor to read Green only
   digitalWrite(S2,HIGH);
   digitalWrite(S3,HIGH);
@@ -78,10 +129,12 @@ int getGreenPW() {
   PW = pulseIn(sensorOut, LOW);
   // Return the value
   return PW;
+
 }
 
 // Function to read Blue Pulse Widths
 int getBluePW() {
+
   // Set sensor to read Blue only
   digitalWrite(S2,LOW);
   digitalWrite(S3,HIGH);
@@ -91,4 +144,5 @@ int getBluePW() {
   PW = pulseIn(sensorOut, LOW);
   // Return the value
   return PW;
+
 }
